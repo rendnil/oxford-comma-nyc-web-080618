68804845eab5c5c0a8170d16af85c3b436470ac5
Andrew Lindner <andrew.lindner.15@gmail.com>
2018-07-16 17:37:14 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,21 @@
 def oxford_comma(array)
 
+  if array.size <=1
+    array.join()
+  elsif array.size == 2
+    array.join(" and ")
+  else
+    last_element = array.pop()
+    
+    shortened_array_string = array.join(", ")
+    
+    
+    full_string = shortened_array_string << ", and #{last_element}"
+    
+    
+  
+  
+  
+  end
+  
 end
